5198b822241aee056e8990b2052b269fd72c40a7
djsreeraj <sreeraj011m@gmail.com>
2021-05-25 23:19:59 +0530

    ejs view setup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello-world");
+  //res.send("Hello-world");
+
+  let name = "URL Shortner";
+  res.render("home", { title: name });
 });
 
 app.get("/short", (req, res) => {
